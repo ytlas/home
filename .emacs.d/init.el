c1a2f056ead855692eac46ecb5674a674c4c9816
ytlas <adam@ytlas.com>
2016-05-10 19:40:56 +0200

    eat da po po

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,10 +1,8 @@
-;;; package --- Summary
-;;; Commentary:
-;; Adam's .emacs config, I know, not the best, but it works superwell!
-;; READ: The config is optimized for XEmacs, everything might not work on terminal emulators like rxvt or xterm (color problems and such).
-;; However, with a tweak or two, it should work perfectly.
+;; Turn off mouse interface early in startup to avoid momentary display
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
-;;; Code:
 ;; Removes GUI crap before anything
 (menu-bar-mode -1)
 (fringe-mode 0)
@@ -111,9 +109,6 @@
 (set-terminal-coding-system 'utf-8)
 (setq locale-coding-system 'utf-8)
 
-;; Shows trailing whitespace
-(setq-default show-trailing-whitespace t)
-
 ;; Sets window title to buffer name
 (setq-default frame-title-format (list "%b"))
 
@@ -209,27 +204,9 @@
 (global-set-key (kbd "M-9") 'comment-region)
 (global-set-key (kbd "C-M-9") 'uncomment-region)
 
-;;; Auto generated code (from customize-group etc.)
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("36d92f830c21797ce34896a4cf074ce25dbe0dabe77603876d1b42316530c99d" "fbcdb6b7890d0ec1708fa21ab08eb0cc16a8b7611bb6517b722eba3891dfc9dd" "2a5be663818e1e23fd2175cc8dac8a2015dcde6b2e07536712451b14658bbf68" "beeb5ac6b65fcccfe434071d4624ff0308b5968bf2f0c01b567d212bcaf66054" "c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "b04425cc726711a6c91e8ebc20cf5a3927160681941e06bc7900a5a5bfe1a77f" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(cursor ((t (:background "dark orange"))))
- '(highlight ((t (:background "gray32"))))
- '(mc/cursor-face ((t (:foreground "gold" :inverse-video t))))
- '(mode-line ((t (:background "white" :foreground "black" :box nil))))
- '(mode-line-inactive ((t (:inherit mode-line :background "dark gray" :foreground "black" :box nil :weight light))))
- '(region ((t (:background "DarkOrange3")))))
+;;; Put auto-generated code in separate file (~/.emacs.d/custom.el)
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file)
 
 ;;; Specific theme settings
 (add-to-list 'default-frame-alist '(foreground-color . "#ffffff"))
