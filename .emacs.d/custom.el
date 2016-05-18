cf1823f3fac8a0fe4efb4d6fd6b8b4db621302ca
ytlas <adam@ytlas.com>
2016-05-18 06:16:57 +0200

    hehe

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -14,7 +14,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Ubuntu Mono" :foundry "unknown" :slant normal :weight normal :height 128 :width normal))))
+ '(default ((t (:family "Ubuntu Mono" :foundry "unknown" :slant normal :weight normal :height 166 :width normal))))
  '(cursor ((t (:background "dark orange"))))
  '(highlight ((t (:background "gray32"))))
  '(mc/cursor-face ((t (:foreground "gold" :inverse-video t))))
