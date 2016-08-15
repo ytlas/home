c04a93d15a7f9d88f9de02d44a55e6e7f64f8975
ytlas <adam@ytlas.com>
2016-08-15 11:21:14 +0200

    :)

--- bin/sp.c
+++ bin/sp.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ncurses.h>
 
 int main(void){
   char option;
-  puts("Are you sure you want to power off the machine (y/N) ");
-  scanf("%c",&option);
+  initscr();
+  raw();
+  printw("Are you sure you want to power off the machine (y/N) ");
+  option=getch();
   if(option=='y'||option=='Y')
     system("sudo poweroff");
+  endwin();
   return 1;
 }
