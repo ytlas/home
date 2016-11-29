3d45ecdf7360c94250e9e851ec98ccf23a87ba96
ytlas <adam@ytlas.com>
2016-11-29 23:50:17 +0100

    :)

--- src/C/astat/bandwidth.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "interface.h"
-
-int main(void){
-  printf("%s",interface());
-}
--- src/C/astat/date.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <time.h>
-char *dateAndTime(void){
-  time_t rawtime;
-  struct tm *timeinfo;
-  time(&rawtime);
-  timeinfo=localtime(&rawtime);
-  return asctime(timeinfo);
-}
