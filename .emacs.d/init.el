cdedfbd3dc7c37597b0255ad156752fcab8c6f29
ytlas <adam@ytlas.com>
2017-02-14 23:26:22 +0100

    :)

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -146,6 +146,7 @@
 (add-to-list 'auto-mode-alist '("\\.ejs\\'"  . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.css\\'"  . css-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
 
 ;;; Key unbindings
 ;; Unbinds annoying keys that I never use
