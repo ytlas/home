4731b2ef0c10d03ddfb95e03251cdfaf09762ca9
ytlas <adam@ytlas.com>
2017-02-20 01:27:54 +0100

    :)

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,26 +3,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(blink-cursor-mode nil)
- '(custom-safe-themes
-   (quote
-    ("beeb5ac6b65fcccfe434071d4624ff0308b5968bf2f0c01b567d212bcaf66054" default)))
- '(jdee-server-dir "~/Documents/jars")
  '(package-selected-packages
    (quote
-    (helm-spotify helm matlab-mode auto-complete exwm flymake-hlint haskell-mode xelb wrap-region web-mode js2-mode iy-go-to-char flycheck expand-region)))
- '(show-paren-mode t))
+    (folding php-mode multi-web-mode flymake-php xelb wrap-region web-mode js2-mode jdee iy-go-to-char helm-spotify haskell-mode flymake-hlint expand-region))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(cursor ((t (:background "deep pink"))))
- '(highlight ((t (:background "gainsboro"))))
- '(mode-line ((t (:background "#131214" :foreground "white" :box (:line-width -1 :color "orange red") :slant normal :weight normal :height 151 :width normal :foundry "misc" :family "fixed"))))
- '(mode-line-inactive ((t (:inherit mode-line :background "grey90" :foreground "black" :box (:line-width -1 :color "#131412") :weight light))))
- '(region ((t (:background "royal blue"))))
- '(web-mode-html-attr-equal-face ((t (:foreground "black"))))
- '(web-mode-html-attr-name-face ((t (:foreground "orange"))))
- '(web-mode-html-tag-bracket-face ((t (:foreground "black"))))
- '(web-mode-html-tag-face ((t (:foreground "dim gray")))))
+ )
--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,19 +4,14 @@
 ;; (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 ;; (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 ;; (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-
 ;; Identification
-(setq user-full-name "ytlas")
-(setq user-mail-address "adam@ytlas.com")
-(add-to-list 'auto-mode-alist '("/mutt" . mail-mode))
+(setq user-mail-address "rabbitfood@posteo.net")
 ;;; Packages setup
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
-(menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
-(fringe-mode 0)
 (display-battery-mode 1)
 
 (defun ensure-package-installed(&rest packages)
@@ -30,12 +25,7 @@
 (or(file-exists-p package-user-dir)
    (package-refresh-contents))
 
-(ensure-package-installed 'expand-region 'flycheck 'iy-go-to-char 'web-mode 'js2-mode 'wrap-region 'haskell-mode 'flymake-hlint)
-
-;; Exwm setup
-;; (require 'exwm)
-;; (require 'exwm-config)
-;; (exwm-config-default)
+(ensure-package-installed 'expand-region 'flycheck 'iy-go-to-char 'multi-web-mode 'web-mode 'js2-mode 'wrap-region 'haskell-mode 'flymake-hlint 'jdee)
 
 ;; Defaults backup files to store in temporary filedirectory (depending on OS)
 (setq backup-directory-alist
@@ -43,26 +33,12 @@
 (setq auto-save-file-name-transforms
       `((".*" ,temporary-file-directory t)))
 
-;;; Defuns
-;; Position cursor at beginning of match when using Isearch
-(add-hook 'isearch-mode-end-hook 'my-goto-match-beginning)
-(defun my-goto-match-beginning ()
-  (when (and isearch-forward isearch-other-end)
-    (goto-char isearch-other-end)))
-(defadvice isearch-exit (after my-goto-match-beginning activate)
-  "Go to beginning of match."
-  (when (and isearch-forward isearch-other-end)
-    (goto-char isearch-other-end)))
-
 ;;; Mode toggles
-;; If there is one or more batteries, display their combined calculated charge remaining
-;; (display-battery-mode)
-
 ;; Column number mode
 (column-number-mode 1)
 
 ;; Ido mode
-(ido-mode 0)
+(ido-mode 1)
 
 ;; Wrap-region mode
 (wrap-region-global-mode)
@@ -77,18 +53,10 @@
 ;; Shows matching paren
 (show-paren-mode 1)
 
+;; Date time format
+(display-time-mode 1)
+
 ;;; Variables
-;; Exwm vars
-;; (setq-default exwm-layout-show-all-buffers t)
-
-;; Displays date and time
-;; (setq display-time-day-and-date t
-;;       display-time-24hr-format t)
-;; (display-time)
-
-;; Makes point move by logical lines
-(setq line-move-visual nil)
-
 ;; Sets the initial scratch buffer content
 (setq initial-scratch-message ";; :)\n")
 
@@ -120,26 +88,17 @@
 ;; Hlint for haskell
 (add-hook 'haskell-mode-hook 'flymake-hlint-load)
 
-;; Exwm hooks
-;; (add-hook 'exwm-manage-finish-hook
-;;	  (lambda ()
-;;	    (when (and exwm-class-name
-;;		       (string= exwm-class-name "URxvt"))
-;;	      (setq-local exwm-input-prefix-keys '(?\C-x)))))
-
 ;; Cleans up trailing whitespace
 (add-hook 'before-save-hook 'whitespace-cleanup)
 (add-hook 'before-save-hook (lambda() (delete-trailing-whitespace)))
 
-;; Enables hl-line-mode in certain major modes
-;; (add-hook 'emacs-lisp-mode-hook 'hl-line-mode)
-;; (add-hook 'sh-mode-hook         'hl-line-mode)
-;; (add-hook 'web-mode-hook        'hl-line-mode)
-;; (add-hook 'python-mode-hook     'hl-line-mode)
-;; (add-hook 'css-mode-hook        'hl-line-mode)
-;; (add-hook 'conf-space-mode-hook 'hl-line-mode)
-;; (add-hook 'c-mode-hook          'hl-line-mode)
-;; (add-hook 'flymake-mode-hook    'hl-line-mode)
+;; ;; Multi-web-mode
+;; (require 'multi-web-mode)
+;; (setq mweb-default-major-mode 'html-mode)
+;; (setq mweb-tags '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
+;;		  (js-mode "<script +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)[^>]*>" "</script>")
+;;		  (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
+;; (setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5"))
 
 ;;; Autoloads
 (add-to-list 'auto-mode-alist '("\\.js\\'"   . js2-mode))
@@ -147,28 +106,6 @@
 (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.css\\'"  . css-mode))
 (add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
-
-;;; Key unbindings
-;; Unbinds annoying keys that I never use
-;; (dolist (k '([mouse-1] [down-mouse-1] [double-mouse-1] [triple-mouse-1] [down-mouse-2] [drag-mouse-2] [double-mouse-2] [triple-mouse-2] [mouse-3] [down-mouse-3] [drag-mouse-3] [double-mouse-3] [triple-mouse-3] [mouse-4] [down-mouse-4] [drag-mouse-4] [double-mouse-4] [triple-mouse-4] [mouse-5] [down-mouse-5] [drag-mouse-5] [double-mouse-5] [triple-mouse-5] [M-down] [M-up]  [M-right]  [M-left]  [C-down]  [C-up]  [C-right]  [C-left]  [down]  [up]  [right] [left] [C-c f] [C-z]))
-;;  (global-unset-key k))
-
-;;; Global key binds
-;; Exwm binds
-;; (exwm-input-set-key (kbd "s-<return>") (lambda ()       (interactive)(start-process "" nil "urxvt")))
-;; (exwm-input-set-key (kbd "s-b")        (lambda ()       (interactive)(start-process "" nil "firefox-esr")))
-;; (exwm-input-set-key (kbd "s-l")        (lambda ()       (interactive)(start-process "" nil "slock")))
-;; (exwm-input-set-key (kbd "s-<left>")   (lambda ()       (interactive)(start-process "" nil "sudo"    "abl"      "-5")))
-;; (exwm-input-set-key (kbd "s-<right>")  (lambda ()       (interactive)(start-process "" nil "sudo"    "abl"      "+5")))
-;; (exwm-input-set-key (kbd "s-<up>")     (lambda ()       (interactive)(start-process "" nil "ponymix" "increase" "5" "--max-volume" "200")))
-;; (exwm-input-set-key (kbd "s-<down>")   (lambda ()       (interactive)(start-process "" nil "ponymix" "decrease" "5" "--max-volume" "200")))
-;; (exwm-input-set-key (kbd "s-d")        (lambda (command)(interactive (list (read-shell-command "$ ")))(start-process-shell-command command nil command)))
-;; (exwm-input-set-key (kbd "s-w")        'kill-buffer-and-window)
-
-;; Flymake
-(global-set-key (kbd "C-c C-e") 'flymake-popup-current-error-menu)
-(global-set-key (kbd "C-c C-n") 'flymake-goto-next-error)
-(global-set-key (kbd "C-c C-p") 'flymake-goto-prev-error)
 
 ;; Windmove
 (global-set-key (kbd "C-c b") 'windmove-left)
@@ -195,6 +132,7 @@
 ;;(add-to-list 'default-frame-alist '(background-color . "#000000"))
 (set-face-attribute 'mode-line nil :font "10x20")
 (set-face-attribute 'default nil :font "-Misc-Fixed-Medium-R-Normal--18-120-100-100-C-90-ISO10646-1")
+
 
 (provide  '.emacs)
 ;;; .emacs ends here
