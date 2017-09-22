9d527c0e9bdf91b9efc8767fddcadc6f6da9e9ca
ytlas <rabbitfood@posteo.net>
2017-09-22 11:03:47 +0200

    :)

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -7,7 +7,7 @@
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
- '(custom-enabled-themes (quote (leuven)))
+ '(custom-enabled-themes nil)
  '(package-selected-packages
    (quote
 	(system-packages rtags tabbar-ruler helm ivy org-bullets which-key try use-package magit w3 auctex php-mode folding tabbar web-mode expand-region))))
