6e4842a7f6ec6f4cd055e05dfc4590df3f092b36
ytlas <rabbitfood@posteo.net>
2017-10-25 20:45:29 +0200

    :)

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -8,17 +8,44 @@
  '(ansi-color-names-vector
    ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
  '(column-number-mode t)
- '(custom-enabled-themes nil)
+ '(custom-enabled-themes (quote (tao-yang)))
+ '(custom-safe-themes
+   (quote
+	("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" "0e0c37ee89f0213ce31205e9ae8bce1f93c9bcd81b1bcda0233061bb02c357a8" "086970da368bb95e42fd4ddac3149e84ce5f165e90dfc6ce6baceae30cf581ef" "f2057733672d3b119791f5b7d1a778bf8880121f22ea122a21d221b45081f49e" default)))
  '(display-time-mode t)
+ '(fci-rule-color "#F1F1F1")
  '(package-selected-packages
    (quote
-	(apt-sources-list hideshow-org fill-column-indicatior flymake-php counsel-projectile consel-projectile system-packages rtags tabbar-ruler helm ivy org-bullets which-key try use-package magit w3 auctex php-mode folding tabbar web-mode expand-region)))
+	(smart-mode-line tao-theme apt-sources-list hideshow-org fill-column-indicatior flymake-php counsel-projectile consel-projectile system-packages rtags tabbar-ruler helm ivy org-bullets which-key try use-package magit w3 auctex php-mode folding tabbar web-mode expand-region)))
  '(show-paren-mode t)
- '(tool-bar-mode nil))
+ '(tool-bar-mode nil)
+ '(vc-annotate-background "#F6F6F6")
+ '(vc-annotate-color-map
+   (quote
+	((20 . "#C3C3C3")
+	 (40 . "#9E9E9E")
+	 (60 . "#9E9E9E")
+	 (80 . "#616161")
+	 (100 . "#616161")
+	 (120 . "#3C3C3C")
+	 (140 . "#3C3C3C")
+	 (160 . "#252525")
+	 (180 . "#252525")
+	 (200 . "#252525")
+	 (220 . "#171717")
+	 (240 . "#171717")
+	 (260 . "#171717")
+	 (280 . "#0E0E0E")
+	 (300 . "#0E0E0E")
+	 (320 . "#0E0E0E")
+	 (340 . "#090909")
+	 (360 . "#090909"))))
+ '(vc-annotate-very-old-color "#3C3C3C"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Ubuntu Mono" :foundry "DAMA" :slant normal :weight normal :height 143 :width normal))))
- '(mode-line ((t (:background "grey75" :foreground "black" :box (:line-width -1 :style released-button) :height 1.1)))))
+ '(default ((t (:inherit nil :stipple nil :background "white smoke" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 143 :width normal :foundry "DAMA" :family "Ubuntu Mono"))))
+ '(mode-line ((t (:background "#616161" :foreground "#FAFAFA" :height 1.1))))
+ '(org-block-background ((t (:foreground "black")))))
